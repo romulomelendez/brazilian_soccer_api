9cd8205c78e3654feeba63437ffc402a9eed6dbd
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-07-10 18:45:45 -0300

    Feat: creating getters and setters

--- src/main/java/com/example/project/api/model/Club.java
+++ src/main/java/com/example/project/api/model/Club.java
@@ -6,4 +6,30 @@ public class Club {
 	private String foundedIn;
 	private String countryState;
 
+    // Getters
+    public String getName() {
+        return this.name;
+    }
+
+    public String getFoundedIn() {
+        return this.foundedIn;
+    }
+
+    public String getCountryState() {
+        return this.countryState;
+    }
+
+    //Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setFoundedIn(String foundedIn) {
+        this.foundedIn = foundedIn;
+    }
+
+    public void setCountryState(String countryState) {
+        this.countryState = countryState;
+    }
+
 }
