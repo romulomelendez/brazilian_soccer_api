0481a6c75ab07c5deb807d9aa1d91e1590c6e073
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-07-10 19:54:44 -0300

    Feat: creating other routes

--- src/main/java/com/example/project/api/controller/ClubController.java
+++ src/main/java/com/example/project/api/controller/ClubController.java
@@ -1,13 +1,23 @@
 package com.example.project.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.project.api.model.Club;
 
 @RestController
 public class ClubController {
     
-    @GetMapping("/")
-    public String getClubName() {
+    @GetMapping("/clubs")
+    public String getAllClubs() {
         return "Hello World!";
+    }
+
+    @GetMapping("/clubs/{clubName}")
+    public String getClub(@PathVariable String clubName) {
+        return clubName;
     }
 }
