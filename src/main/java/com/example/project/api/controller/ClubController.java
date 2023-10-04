e92f95fe5a887b4025aaf869b8d1eae41d42317a
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-10-04 20:26:59 -0300

    Feat: save club data from frontend in database

--- src/main/java/com/example/project/api/controller/ClubController.java
+++ src/main/java/com/example/project/api/controller/ClubController.java
@@ -1,5 +1,6 @@
 package com.example.project.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,9 +8,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.project.api.model.Club;
+import com.example.project.api.repository.ClubRepository;
 
 @RestController
 public class ClubController {
+
+    @Autowired
+    private ClubRepository repo;
     
     @GetMapping("/clubs")
     public String getAllClubs() {
@@ -23,9 +28,6 @@ public class ClubController {
 
     @PostMapping("/create/club")
     public Club createClub(@RequestBody Club clubData) {
-
-        Club club = new Club();
-
-        return club;
+        return repo.save(clubData);
     }
 }
