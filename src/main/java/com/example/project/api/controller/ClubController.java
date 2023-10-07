4e264b1a1eefe580227c13fa3cdb8711de4dbefb
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-10-07 20:53:25 -0300

    feat: update club data route implemented

--- src/main/java/com/example/project/api/controller/ClubController.java
+++ src/main/java/com/example/project/api/controller/ClubController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +30,10 @@ public class ClubController {
     @PostMapping("/create/club")
     public Club createClub(@RequestBody Club clubData) {
         return repo.save(clubData);
+    }
+
+    @PutMapping("/clubs/update")
+    public Club updateClub(@RequestBody Club newClubData) {
+        return repo.save(newClubData);
     }
 }
