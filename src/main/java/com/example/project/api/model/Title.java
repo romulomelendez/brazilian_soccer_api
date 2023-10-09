36108a03ee0a6d22cf3ac975040b4f6f9b187e4b
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-10-09 06:47:25 -0300

    Feat: title entity class attrubutes updated

---
+++ src/main/java/com/example/project/api/model/Title.java
@@ -0,0 +1,56 @@
+package com.example.project.api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "title")
+public class Title {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", length = 30, nullable = false)
+    private String name;
+    
+    @Column(name = "winning_year", length = 5, nullable = false)
+    private Integer winningYear;
+    
+    @Column(name = "description", length = 150, nullable = true)
+    private String description;
+    
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getWinningYear() {
+        return winningYear;
+    }
+
+    public void setWinningYear(Integer winningYear) {
+        this.winningYear = winningYear;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
